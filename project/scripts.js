4ec91b3c3c7c80ca5be41a718d1fafb1c0ac98e0
shan wolff <shanwolff182@gmail.com>
2023-10-25 17:17:57 -0400

    finished

--- project/scripts.js
+++ project/scripts.js
@@ -1,80 +1,119 @@
-//Begin Button
-//variables
 let person = "";
-let budget = Number(0);
-let needs = Number(0);
-let savings = Number(0);
-let wants = Number(0);
+let budget = 0;
+let savings = 0;
+let needs = 0;
+let wants = 0;
+
+let beginButton = document.getElementById("begin-button");
+beginButton.addEventListener("click", myBudget);
+
+//budget/start paragraphs
+let budgetInfoElement = document.getElementById("budget-info");
+let printNeeds = document.getElementById("needs");
+let printSavings = document.getElementById("savings");
+let printWants = document.getElementById("wants");
+
+//needs paragraphs
+let updatedNeedsInfo = document.getElementById("needs-info-needs");
+let updatedSavingsInfo = document.getElementById("needs-info-savings");
+let updatedWantsInfo = document.getElementById("needs-info-wants");
+let printOverBudget = document.getElementById("needs-info");
+let printWithinBudget = document.getElementById("needs-info");
+//emergency fund paragraphs
+let emergencyTableNeeds = document.getElementById("emergency-fund-needs");
+let emergencyTableSavings = document.getElementById("emergency-fund-savings");
+let emergencyTableWants = document.getElementById("emergency-fund-wants");
+let emergencyTableEmergency = document.getElementById("emergency-fund-emergency")
+
+
 
 function myBudget() {
-    person = prompt("Please enter your name or ID");
-    budget = prompt("Please enter your monthly salary");
-    needs = budget * .5;
-    savings = budget * .2;
-    wants = budget * .3;
+    person = prompt("Enter your name");
+    budget = prompt("Enter your monthly income");
+    needs = 0.5 * budget;
+    savings = 0.2 * budget;
+    wants = 0.3 * budget;
+    
+    budgetInfoElement.textContent = `Hello ${person}! Here is your budget for this month:`;
+    printNeeds.textContent = `Needs: ${needs}`;
+    printSavings.textContent = `Savings: ${savings}`;
+    printWants.textContent = `Wants: ${wants}`;
+};
 
-    userInput = document.getElementById("part1");
 
-    helloMessage = document.createElement("p");
-    helloMessage.textContent = `Hello ${person}! Here is your budget for this month: $ ${budget}`;
-    userInput.appendChild(helloMessage);
+let food = 0;
+let housing = 0;
+let bills = 0;
+let healthcare = 0;
+let transportation = 0;
+let otherEssentials = 0;
 
-    userNeeds = document.createElement("p");
-    userNeeds.textContent = `Needs: $${needs}`;
-    userInput.appendChild(userNeeds);
-
-    userSavings = document.createElement("p");
-    userSavings.textContent = `Savings: $${savings}`;
-    userInput.appendChild(userSavings);
-
-    userWants = document.createElement("p");
-    userWants.textContent = `Wants: $${wants}`;
-    userInput.appendChild(userWants);
-
-}
-
-document.getElementById("addButton").addEventListener("click", myBudget);
+inputNeedsButton = document.getElementById("needs-button");
+inputNeedsButton.addEventListener("click", inputYourNeeds);
 
 function inputYourNeeds() {
+    food = prompt("Enter total monthly food costs");
+    housing = prompt("Enter total monthly housing costs");
+    bills = prompt("Enter total monthly bill costs");
+    healthcare = prompt("Enter total monthly healthcare costs");
+    transportation = prompt("Enter total monthly transportation costs");
+    otherEssentials = prompt("Enter total monthly costs for other essentials");
+    needs = needs - food - housing - bills - healthcare - transportation - otherEssentials;
+    if (needs < 0) {
+        printOverBudget.textContent = `You are over your needs budget by ${needs}. You will need to subtract from your wants budget for the month.`;
+        wants = needs + wants  ;
+        updatedNeedsInfo.textContent = `Needs: ${needs}`;
+        updatedSavingsInfo.textContent = `Savings: ${savings}`;
+        updatedWantsInfo.textContent = `Wants: ${wants}`;
+    } else {
+        printWithinBudget.textContent = `You are doing great!`;
+        updatedNeedsInfo.textContent = `Needs: ${needs}`;
+        updatedSavingsInfo.textContent = `Savings: ${savings}`;
+        updatedWantsInfo.textContent = `Wants: ${wants}`;
+    };
+};
 
-}
+let emergencyButton = document.getElementById("emergency-button");
+emergencyButton.addEventListener("click", emergencyFund);
 
-document.getElementById("addNeedButton").addEventListener("click", inputYourNeeds);
+function emergencyFund() {
+    let funds = prompt("Enter a monthly amount you would like to put towards your Emergency Fund.");
+    if (funds > savings) {
+    funds = prompt("That’s too much. Try a smaller amount");
+    savings = savings - funds;
+    } else {
+    savings = savings - funds;
+    }
+    emergencyTableNeeds.textContent = `Needs: ${needs}`;
+    emergencyTableSavings.textContent = `Savings: ${savings}`;
+    emergencyTableWants.textContent = `Wants: ${wants}`;
+    emergencyTableEmergency.textContent = `Emergency: ${funds}`;
+};
 
+let startOverButton = document.getElementById("start-over-button");
+startOverButton.addEventListener("click", startOver);
 
+    function startOver() {
+    person = "";
+    budget = 0;
+    savings = 0;
+    needs = 0;
+    wants = 0;
 
+    budgetInfoElement.textContent = ""
+    printNeeds.textContent = ""
+    printSavings.textContent = ""
+    printWants.textContent = ""
 
-// document.addEventListener()
-// addButton.addEventListener('click', function () {})
+    updatedNeedsInfo.textContent = ""
+    updatedSavingsInfo.textContent = ""
+    updatedWantsInfo.textContent = ""
+    printOverBudget.textContent = ""
+    printWithinBudget.textContent = ""
 
+    emergencyTableNeeds.textContent = ""
+    emergencyTableSavings.textContent = ""
+    emergencyTableWants.textContent = 
+    emergencyTableEmergency.textContent = ""
 
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     console.log('skgjnskjgn')
-//     const addButton = document.getElementById('addButton');
-//     const newTaskInput = document.getElementById('newTask');
-//     const taskList = document.getElementById('taskList');
-
-//     addButton.addEventListener('click', function () {
-//         const taskText = newTaskInput.value;
-
-//         if (taskText !== '') {
-//             const taskItem = document.createElement('li');
-//             taskItem.textContent = taskText;
-
-//             taskItem.addEventListener('click', function () {
-//                 taskItem.classList.toggle('completed');
-//             });
-
-//             taskList.appendChild(taskItem);
-//             newTaskInput.value = '';
-//         }
-//     });
-// });
-
-
-
+    };
